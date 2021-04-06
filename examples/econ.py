8363fa6c5aa4dc2e574a0b2e578b482f94c5ff19
Martin Staadecker <machstg@gmail.com>
2021-04-05 22:29:33 -0400

    Add factors

--- examples/econ.py
+++ examples/econ.py
@@ -38,21 +38,18 @@ def find_yield(P, A, N, F):
 
 @register_formula([
     NumInput("number of periods"),
-    NumInput("rate (percent)")
+    PercentInput("rate")
 ])
 def annuity_factor(N, r):
-    r /= 100
     factor = (1 - (1 + r) ** (-N)) / r
     return {"P/A": factor, "A/P": 1 / factor}
 
 @register_formula([
     NumInput("number of periods"),
-    NumInput("rate (percent)"),
-    NumInput("gradient rate (percent)")
+    PercentInput("rate"),
+    PercentInput("gradient rate")
 ], name="factors.geometric gradient")
 def geometric_factor(N, i, g):
-    i /= 100
-    g /= 100
     factor = (((1 + g) / (1 + i)) ** N - 1) / (g - i)
     return {"P/geom": factor, "geom/P": 1 / factor}
 
@@ -60,10 +57,35 @@ def geometric_factor(N, i, g):
 @register_formula([
     PercentInput("tax rate (t)"),
     PercentInput("CCA (depreciation) rate (d)"),
-    PercentInput("interest rate (normally after-tax MARR/IRR) (i)")
+    PercentInput("interest rate (normally after-tax actual MARR/IRR) (i)")
 ], name="factors.CRF/CTF")
 def tax_factors(t, d, i):
     return {"CRF": 1 - t * d / (i + d), "CTF": 1 - t * d / (i + d) * (1 + i / 2) / (1 + i)}
+
+
+@register_formula([
+    NumInput("number of periods (n)"),
+    PercentInput("rate (percent)")
+], name="factors.annual")
+def annual_factor(N, i):
+    factor = (1 - (1+i) ** (-N)) / i
+    return {"P/A": factor, "A/P": 1 / factor}
+
+@register_formula([
+    NumInput("n"),
+    PercentInput("rate")
+], name="factors.future")
+def future_factor(N, i):
+    factor = (1+i) ** N
+    return { "P/F": 1 / factor , "F/P": factor }
+
+@register_formula([
+    NumInput("n"),
+    PercentInput("rate")
+], name="factors.arithmetic_grad")
+def arithmetic_gradient_factor(N, i):
+    factor = (1 - (1 + i * N) / (1 + i) ** N) / (i ** 2)
+    return { "P/G": factor, "G/P": 1 / factor }
 
 
 if __name__ == "__main__":
